86e4550268dec4e49d5450d54c8dd8d2e22d3f3a
Mukul Kedia <mucookul@gmail.com>
2025-07-22 09:25:53 -0400

    chore: fix

--- rslib.config.ts
+++ rslib.config.ts
@@ -5,12 +5,10 @@ import path from "node:path";
 const isProduction = process.argv.includes("production");
 
 const modifyFiles = await glob("src/modifyFiles/**/*", { nodir: true });
-const modifyFilesEntries = Object.fromEntries(
-  modifyFiles.map((file) => {
-    const entryName: string = file.split(path.sep).slice(1).join(path.sep);
-    return [entryName, file];
-  }),
-);
+const modifyFilesEntries = modifyFiles.map((file) => {
+  const entryName: string = file.split(path.sep).slice(1).join(path.sep);
+  return { from: file, to: entryName };
+})
 
 export default defineConfig({
   lib: [
@@ -23,11 +21,14 @@ export default defineConfig({
   source: {
     entry: {
       extension: "./src/extension.ts",
-      ...modifyFilesEntries,
     },
   },
   output: {
     cleanDistPath: true,
+    copy: modifyFilesEntries,
+    distPath: {
+      root: "./dist/"
+    },
     externals: ["vscode", "electron"],
     minify: isProduction,
     sourceMap: !isProduction,
